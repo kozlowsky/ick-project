bb1057f63b4aaef5f43a1d3d93387af6c4213b23
Jakub Kozlowski <jkozlowski094@gmail.com>
2018-10-15 23:41:04 +0200

    secret added to webpage

--- src/router.js
+++ src/router.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 import HomePage from './components/HomePage'
 import Login from './components/Login'
 import Register from './components/Register'
+import Cats from './components/Cats'
 
 Vue.use(Router)
 
@@ -22,6 +23,11 @@ export default new Router({
             path: '/register',
             name: 'Register',
             component: Register
+        },
+        {
+            path: '/secret',
+            name: 'Cats',
+            component: Cats
         }
     ]
 })
