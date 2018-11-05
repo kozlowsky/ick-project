b66a071a3c957ded7e9f6278ad8de365c54d68c7
Maksymilian <paluszekmaksymilian@gmail.com>
2018-11-05 19:24:27 +0100

    early fake ad store

---
+++ src/store/modules/ads.js
@@ -0,0 +1,44 @@
+const state = {
+    ads: [
+        {
+            id: 1,
+            advertiser: 'Jan Kowalski',
+            title: 'Super kot',
+            type: 'adopcja',
+            description: 'Mam na sprzedaż kota, nówka sztuka nie śmigana',
+            address: 'Warszawa, Kocia 12',
+            species: 'Kot',
+            gender: 'Female',
+            age: 2,
+            colors: ['czarny', 'biały'],
+            listOfVaccines: ['kocia grypa'],
+            listOfDiseases: ['czarna śmierć']
+        },
+        {
+            id: 2,
+            advertiser: 'Schronisko im. Kleksa we Wrocławiu',
+            title: 'Piesek Leszek',
+            type: 'Wirtualna Adopcja',
+            description: 'Główna postać ze znajej kreskówki nie radzi sobie z końcem kariery' +
+                'prosi o wsparcie',
+            address: 'Wrocław, Psia 12',
+            species: 'Pies',
+            gender: 'Male',
+            age: 15,
+            colors: ['czerwony'],
+            listOfVaccines: [],
+            listOfDiseases: []
+        }
+        ]
+}
+
+const getters = {
+    ads (state) {
+        return state.ads
+    }
+}
+
+export default {
+    state,
+    getters
+}
--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import ads from './modules/ads'
 
 Vue.use(Vuex)
 
@@ -12,5 +13,8 @@ export default new Vuex.Store({
   },
   actions: {
 
+  },
+  modules: {
+    ads
   }
 })
