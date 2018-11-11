95e8f453772ce532dce58246b1fa31943604371c
Konrad Drozd <drozdkonrad@gmail.com>
2018-11-11 16:03:44 +0100

    Add missing :key in v-for

--- src/components/SearchPanel.vue
+++ src/components/SearchPanel.vue
@@ -1,59 +1,59 @@
 <script>
-    import between from "vuelidate/src/validators/between";
-    import numeric from "vuelidate/src/validators/numeric";
+import between from "vuelidate/src/validators/between";
+import numeric from "vuelidate/src/validators/numeric";
 
-    export default {
-        name: "SearchPanel",
-        data() {
-            return {
-                specie: '',
-                species: [],
+export default {
+    name: "SearchPanel",
+    data () {
+        return {
+            specie: '',
+            species: [],
 
-                gender: '',
-                genders: [
-                    'Samiec',
-                    'Samica',
-                    'Obojętne'
-                ],
+            gender: '',
+            genders: [
+                'Samiec',
+                'Samica',
+                'Obojętne'
+            ],
 
-                breed: '',
-                breeds: [],
-                currentBreeds: [],
+            breed: '',
+            breeds: [],
+            currentBreeds: [],
 
 
-                colors: [],
-                age: 0
+            colors: [],
+            age: 0
 
-            }
-        },
-        methods: {
-            setBreeds() {
-                let index = this.species.indexOf(this.specie);
-                this.currentBreeds = this.breeds[index];
-            }
-        },
-        validations: {
-            age: {
-                between: between(0, 30),
-                numeric,
-            }
-        },
-        computed: {
-            ageErrors() {
-                const errors = [];
-                if (!this.$v.age.$dirty) return errors;
-                !this.$v.age.numeric && errors.push('Niepoprawny wiek');
-                !this.$v.age.between && errors.push('Niepoprawny wiek');
-                return errors;
-
-            }
-        },
-        created() {
-            this.species = this.$store.getters.getSpecies;
-            this.colors = this.$store.getters.getColors;
-            this.breeds = this.$store.getters.getBreeds;
         }
+    },
+    methods: {
+        setBreeds () {
+            let index = this.species.indexOf(this.specie);
+            this.currentBreeds = this.breeds[index];
+        }
+    },
+    validations: {
+        age: {
+            between: between(0, 30),
+            numeric,
+        }
+    },
+    computed: {
+        ageErrors () {
+            const errors = [];
+            if (!this.$v.age.$dirty) return errors;
+            !this.$v.age.numeric && errors.push('Niepoprawny wiek');
+            !this.$v.age.between && errors.push('Niepoprawny wiek');
+            return errors;
+
+        }
+    },
+    created () {
+        this.species = this.$store.getters.getSpecies;
+        this.colors = this.$store.getters.getColors;
+        this.breeds = this.$store.getters.getBreeds;
     }
+}
 </script>
 
 <template>
@@ -63,53 +63,42 @@
         </v-card-title>
         <v-card-text>
             <v-layout row>
-                <v-autocomplete
-                        v-model="specie"
-                        label="Wpisz gatunek pupila"
-                        :items="species"
-                        @change="setBreeds">
+                <v-autocomplete v-model="specie" label="Wpisz gatunek pupila" :items="species" @change="setBreeds">
                 </v-autocomplete>
             </v-layout>
             <v-layout row>
-                <v-select v-model="breed" :items="currentBreeds" :disabled="specie === ''"
-                          label="Wpisz gatunek/rasę zwierzęcia">
+                <v-select v-model="breed" :items="currentBreeds" :disabled="specie === ''" label="Wpisz gatunek/rasę zwierzęcia">
                 </v-select>
             </v-layout>
             <v-layout row>
                 <v-flex xs12 md4>
                     <p class="subheading">Płeć</p>
                     <v-radio-group v-model="gender">
-                        <v-radio v-for="el in genders"
-                                 :label="`${el}`"
-                                 :value="el"
-                        ></v-radio>
+                        <v-radio v-for="el in genders" :key="el" :label="`${el}`" :value="el"></v-radio>
                     </v-radio-group>
                 </v-flex>
                 <v-flex xs12 md4>
                     <p class="subheading">Barwa</p>
                     <v-radio-group>
-                        <v-radio v-for="el in colors" :label="`${el}`"
-                                 :value="el">
+                        <v-radio v-for="el in colors" :key="el" :label="`${el}`" :value="el">
                         </v-radio>
                     </v-radio-group>
                 </v-flex>
                 <v-flex xs12 md4>
                     <p class="subheading">Wiek</p>
-                    <v-text-field v-model="age" :error-messages="ageErrors" @blur="$v.age.$touch()"
-                                  @input="$v.age.$touch()">
+                    <v-text-field v-model="age" :error-messages="ageErrors" @blur="$v.age.$touch()" @input="$v.age.$touch()">
                     </v-text-field>
                 </v-flex>
 
             </v-layout>
         </v-card-text>
         <v-card-actions>
-            <v-spacer/>
+            <v-spacer />
             <v-btn :disabled="specie === ''" color="success">Szukaj</v-btn>
-            <v-spacer/>
+            <v-spacer />
         </v-card-actions>
     </v-card>
 </template>
 
 <style scoped>
-
 </style>
--- src/components/adverts/content/AnimalCards.vue
+++ src/components/adverts/content/AnimalCards.vue
@@ -21,7 +21,7 @@
 <template>
     <v-container fluid grid-list-xl>
         <p class="headline">Karta chorób</p>
-        <v-layout row wrap v-for="disease in advert.listOfDiseases">
+        <v-layout row wrap v-for="disease in advert.listOfDiseases" :key="disease.name">
             <v-flex xs12 md2>
                 <p class="font-weight-medium">{{ disease.interval }}</p>
             </v-flex>
@@ -34,7 +34,7 @@
             </v-flex>
         </v-layout>
         <p class="headline">Karta szczepień</p>
-        <v-layout row wrap v-for="vaccine in advert.listOfVaccines">
+        <v-layout row wrap v-for="vaccine in advert.listOfVaccines" :key="vaccine.name">
             <v-flex xs12 md2>
                 <p class="font-weight-medium">{{ vaccine.date }}</p>
             </v-flex>
